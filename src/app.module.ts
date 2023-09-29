f9dbf4dbdb719d491c31345b12184ba769ca184c
Kurt Maglalang <kbm126@live.com>
2023-09-28 18:09:27 -0700

    feat: add other moduesl to main app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { BookmarkModule } from './bookmark/bookmark.module';
 
+// ! module decorator for the class - required to indicator if it is a module or not
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule, UserModule, BookmarkModule], // other modulres are added automatically with the nest.js generator
 })
 export class AppModule {}
+
+// ! this is the main module that will import the other modules
