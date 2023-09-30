64fe593a4a93b07189c6e8df13abfa35c75ba951
Kurt Maglalang <kbm126@live.com>
2023-09-29 19:03:14 -0700

    setup code for the sign up

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, ParseIntPipe, Post, Req } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { Request } from 'express';
+import { AuthDto } from './dto';
 
 // ! controller will handle the requests and responses - will handle the body of the request, check the header, or any work related to the request
 
@@ -11,10 +13,31 @@ export class AuthController {
   }
 
   @Post('signup') // * POST auth/signup
-  signUp() {
-    // return { response: 'I am signup' }; // ! nest js automatically handles content type depending on what is used
-    // return 'I am signup';
-    return this.authService.signUp();
+  // signUp(@Req() req: Request) {
+  // ! DON'T USE THE REQUEST OBJECT OF THE UNDERLYING LIBRARY (EXPRESS FASTIFY) IN CASE YOU WANT TO SWITCH TO THE OTHER
+  // return { response: 'I am signup' }; // ! nest js automatically handles content type depending on what is used
+  // return 'I am signup';
+  // console.log(req.body);
+  signUp(
+    // @Body('email') email: string,
+    // @Body('password') password: string,
+    // @Body('password', ParseIntPipe) password: string, // ! with the parseintpipe added and expecting for a number, it will throw an error and send a message back to the client
+    @Body() dto: AuthDto,
+  ) {
+    //! use this instead, since nestjs will get the body for you
+    /* The line `console.log('🚀 ~ file: auth.controller.ts:20 ~ signUp ~ dto:', dto);` is used to log
+    the value of the `dto` variable to the console. This can be helpful for debugging purposes, as
+    it allows you to see the contents of the `dto` object when the `signUp` method is called. */
+    // console.log('🚀 ~ file: auth.controller.ts:20 ~ signUp ~ dto:', dto);
+
+    // console.log({
+    //   email,
+    //   typoeOfEmail: typeof email,
+    //   password,
+    //   typeOfPassword: typeof password,
+    // });
+
+    return this.authService.signUp(dto);
   }
 
   @Post('signin') // * POST auth/signin
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
 // import { User, Bookmark } from '@prisma/client'; // ! prisma auto generates the types of the models
+import * as argon from 'argon2'; // ! use this since it is better than bcrypt
 
 // ! create a matching service fucntion here to be used in the controller
 // ! do all the business logic here - connecting to the database, editing fields, etc>>>
@@ -7,9 +10,33 @@ import { Injectable } from '@nestjs/common';
 @Injectable({}) // ! for dependency injection
 export class AuthService {
   // test() {}
+  constructor(private prisma: PrismaService) {} // ! throws an error: nest js does not know what that is even if it is in the auth module, the prisma module has no exported it to other providers
 
-  signUp() {
-    return { msg: 'I have signed up' };
+  async signUp(dto: AuthDto) {
+    // generate the password hash
+    const hash = await argon.hash(dto.password);
+
+    // save the new user in the db
+    const user = await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        hash,
+      },
+      // select: { //! would usually use something like transformers instead
+      //   //  this is one option to limit the return data sent back to the client
+      //   id: true,
+      //   email: true,
+      //   createdAt: true,
+      // },
+    });
+
+    delete user.hash; // ! this is quick and dirty
+
+    // return the saved user
+    return user;
+    // ! note the hash should not be returned to the client
+
+    // return { msg: 'I have signed up' };
   }
 
   signin() {
