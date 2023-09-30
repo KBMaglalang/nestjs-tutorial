d3ee9a7f342f29bed259c9e5a2d4433f71ecb2d4
Kurt Maglalang <kbm126@live.com>
2023-09-29 23:23:16 -0700

    setup the use of env imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
@@ -6,7 +8,13 @@ import { PrismaModule } from './prisma/prisma.module';
 
 // ! module decorator for the class - required to indicator if it is a module or not
 @Module({
-  imports: [AuthModule, UserModule, BookmarkModule, PrismaModule], // other modulres are added automatically with the nest.js generator
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }), //! uses the dontenv library underneath // need to add the global decorator since it is imported in app but need to be used in the prisma service
+    AuthModule,
+    UserModule,
+    BookmarkModule,
+    PrismaModule,
+  ], // other modulres are added automatically with the nest.js generator
 })
 export class AppModule {}
 
