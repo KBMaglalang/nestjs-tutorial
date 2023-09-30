35f2adbcdd914af5041bb8f352c467e9361b8fbb
Kurt Maglalang <kbm126@live.com>
2023-09-29 19:02:15 -0700

    add validator pipe to the server and set whitelist

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // ! use this to limit the expected fields in the request body to what you are expecting, what is shown in the dto
+    }),
+  ); // ! this needs to be added to use the class validator and class transformer
+
   await app.listen(3333);
 }
 bootstrap();
