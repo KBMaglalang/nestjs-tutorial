b6b883627df51508c90fb54201fbe98e060cf861
Kurt Maglalang <kbm126@live.com>
2023-10-01 22:41:53 -0700

    add code for the controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,14 +1,18 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Patch, Req, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { User } from '@prisma/client';
 import { Request } from 'express';
 
 import { GetUser } from '../auth/decorator';
 import { JwtGuard } from '../auth/guard';
+import { EditUserDto } from './dto';
+import { UserService } from './user.service';
 
 @UseGuards(JwtGuard) // ! require an identity in the user controller on a global level for the users route
 @Controller('users')
 export class UserController {
+  constructor(private userService: UserService) {}
+
   // @UseGuards(AuthGuard('jwt')) // ! passport already has a guard you can use along with your strategy
   // @UseGuards(JwtGuard) // ! setting up a guard class
   @Get('me') // /users/me
@@ -22,5 +26,11 @@ export class UserController {
 
     // return 'user info';
     return user;
+  }
+
+  @Patch()
+  editUser(@GetUser('id') userId: number, @Body() dto: EditUserDto) {
+    // ! the effect of returning the specified key was set in the decorator file
+    return this.userService.editUser(userId, dto);
   }
 }
